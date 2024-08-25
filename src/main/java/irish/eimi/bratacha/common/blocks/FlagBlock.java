ac30b54a1818e4cd27d9579f0c48c01fda9d72e8
Katrina Knight <kat.knight@newgaea.net>
2024-08-25 18:01:28 +0100

    feat(flag): add correct collision to the flag blocks

--- src/main/java/irish/eimi/bratacha/common/blocks/FlagBlock.java
+++ src/main/java/irish/eimi/bratacha/common/blocks/FlagBlock.java
@@ -15,12 +15,14 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
-public class FlagBlock extends DirectionalBlock implements EntityBlock {
-
+public class FlagBlock extends HorizontalDirectionalBlock implements EntityBlock {
 
     public static final MapCodec<FlagBlock> CODEC = RecordCodecBuilder.mapCodec((builder) -> {
         return builder.group(
@@ -29,13 +31,32 @@ public class FlagBlock extends DirectionalBlock implements EntityBlock {
                 }), propertiesCodec()).apply(builder, FlagBlock::new);
     });
 
+    protected final VoxelShape[] collisionShapeByIndex;
     private ResourceLocation flagType;
     public FlagBlock(ResourceLocation flag,BlockBehaviour.Properties properties ) {
         super(properties);
         this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.SOUTH));
         this.flagType = flag;
+        this.collisionShapeByIndex = this.makeShapes();
     }
 
+    private VoxelShape[] makeShapes() {
+        VoxelShape up = Block.box(0,0,0,16,1,16);
+        VoxelShape east = Block.box(6,0,0,9,16,16); // fix
+        VoxelShape west = Block.box(7,0,0,10,16,16);
+
+        VoxelShape south = Block.box(0,0,6,16,16,9);
+        VoxelShape north = Block.box(0,0,7,16,16,10);
+
+        VoxelShape[] avoxelshape={up,up,north,south,west,east};
+
+        return avoxelshape;
+    }
+
+    @Override
+    protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return this.collisionShapeByIndex[state.getValue(FACING).ordinal()];
+    }
     @Override
     public @Nullable BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
         return new FlagBlockEntity(blockPos,blockState);
@@ -68,18 +89,13 @@ public class FlagBlock extends DirectionalBlock implements EntityBlock {
 
 
     @Override
-    protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
-        return super.getShape(state, level, pos, context);
-    }
-
-    @Override
     protected MapCodec<? extends FlagBlock> codec() {
         return CODEC;
     }
 
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
-        return this.defaultBlockState().setValue(FACING, context.getNearestLookingDirection().getOpposite().getOpposite());
+       return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
     }
 
     public ResourceLocation getFlagType() {
